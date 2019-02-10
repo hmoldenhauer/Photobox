93b5beabdf1aba9ac695cdac112562ff3cfaa4b7
hmoldenhauer <henning.moldehauer@udo.edu>
2019-02-10 13:53:50 +0100

    added comments

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 WANTED=gvfsd-gphoto2
 
+# check if WANTED exists and kill it
 if pidof $WANTED > /dev/null ; then
     echo "$WANTED is going to be killed"
     $(kill $(pidof $WANTED))
@@ -8,5 +9,6 @@ else
     echo "$WANTED has already been killed"
 fi
 
+# start the scripts for the Photobox
 echo "starting the Photobox"
 $(python photo.py & python telegram.py & FLASK_APP=webinterface.py flask run &)
